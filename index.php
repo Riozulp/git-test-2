b2c28927e9ea2005e91e3c0dad5c726b11e9c103
Riozulp <pambudi892@gmail.com>
2022-01-04 21:31:37 +0700

    Menambahkan paragraf

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 </head>
 <body>
    <h1>Hello Wordl</h1>
+   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos esse quidem autem rerum nihil quod temporibus magnam perferendis quam itaque corrupti ex necessitatibus eveniet, illum inventore, eos, perspiciatis molestias beatae.</p>
 </body>
 </html>
